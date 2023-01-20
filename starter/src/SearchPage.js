c5f756bf07bcef528f2a369f16a0867434150d14
mazen <es-mazensalah2025@alexu.edu.eg>
2023-01-20 05:57:03 +0200

    Update SearchPage.js

--- starter/src/SearchPage.js
+++ starter/src/SearchPage.js
@@ -1,49 +1,69 @@
-import React, { useState, useEffect } from 'react';
-import * as BooksAPI from './BooksAPI';
-import Book from './Book';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import * as BooksAPI from "./BooksAPI";
+import Book from "./Book";
+import "./App.css";
 
 function SearchPage({ history }) {
-  const [query, setQuery] = useState('');
+  const [query, setQuery] = useState("");
   const [books, setBooks] = useState([]);
+  const [myBooks, setMyBooks] = useState([]);
 
   useEffect(() => {
     searchBooks(query);
   }, [query]);
 
+  useEffect(() => {
+    BooksAPI.getAll().then((books) => {
+      setMyBooks(books);
+    });
+  }, []);
+
   const updateQuery = (q) => {
     setQuery(q);
-  }
+  };
 
   const searchBooks = (q) => {
-    if(q) {
+    if (q) {
       BooksAPI.search(q).then((books) => {
-        if(books.length > 0){
+        books.map((book) => (book.shelf = "none"));
+        books.map((book) => {
+          myBooks.map((myBook) => {
+            if (myBook.id === book.id) {
+              book.shelf = myBook.shelf;
+            }
+          });
+        });
+
+        if (books.length > 0) {
           setBooks(books);
-        }else{
+        } else {
           setBooks([]);
         }
       });
-    }else{
+    } else {
       setBooks([]);
     }
-  }
+  };
 
   const handleShelfChange = (book, shelf) => {
     BooksAPI.update(book, shelf).then(() => {
-      setBooks(state => state.map(b => {
-        if(b.id === book.id) {
-          b.shelf = shelf
-        }
-        return b;
-      }))
+      setBooks((state) =>
+        state.map((b) => {
+          if (b.id === book.id) {
+            b.shelf = shelf;
+          }
+          return b;
+        })
+      );
     });
-  }
+  };
 
   return (
     <div className="search-books">
       <div className="search-books-bar">
-        <a className="close-search" href='/'>Close</a>
+        <a className="close-search" href="/">
+          Close
+        </a>
         <div className="search-books-input-wrapper">
           <input
             type="text"
@@ -55,11 +75,17 @@ function SearchPage({ history }) {
       </div>
       <div className="search-books-results">
         <ol className="books-grid">
-          {books.map((book) => (
-            <li key={book.id}>
-              <Book book={book} onShelfChange={handleShelfChange} />
-            </li>
-          ))}
+          {books.map((book) => {
+            const myBook = myBooks.find((b) => b.id === book.id);
+            if (myBook) {
+              book.shelf = myBook.shelf;
+            }
+            return (
+              <li key={book.id}>
+                <Book book={book} onShelfChange={handleShelfChange} />
+              </li>
+            );
+          })}
         </ol>
       </div>
     </div>
@@ -68,3 +94,9 @@ function SearchPage({ history }) {
 
 export default SearchPage;
 
+//to make the default state of the shelf "none" if the book is not in the user's book list (myBooks) add the following code to the searchBooks function:
+// if(myBook) {
+//   book.shelf = myBook.shelf;
+// }else{
+//   book.shelf = 'none';
+// }
