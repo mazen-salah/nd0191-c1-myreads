e1b905450b1c384e24a8ff336f9a75230bda059e
mazen <es-mazensalah2025@alexu.edu.eg>
2023-01-20 04:24:17 +0200

    Update App.js

--- starter/src/App.js
+++ starter/src/App.js
@@ -1,18 +1,16 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Route } from 'react-router-dom';
 import MainPage from './MainPage';
 import SearchPage from './SearchPage';
 import './App.css';
 
-class App extends Component {
-  render() {
-    return (
-      <div>
-        <Route exact path="/" component={MainPage} />
-        <Route exact path="/search" component={SearchPage} />
-      </div>
-    );
-  }
+function App() {
+  return (
+    <div>
+      <Route exact path="/" component={MainPage} />
+      <Route exact path="/search" component={SearchPage} />
+    </div>
+  );
 }
 
 export default App;
