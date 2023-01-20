be3f3eaff61ad1a9971745c1fa7ce8bfdf8da6c8
mazen <es-mazensalah2025@alexu.edu.eg>
2023-01-20 04:39:20 +0200

    Update index.js

--- starter/src/index.js
+++ starter/src/index.js
@@ -1,16 +1,12 @@
 import React from 'react';
-import { Route } from 'react-router-dom';
-import MainPage from './MainPage';
-import SearchPage from './SearchPage';
-import './App.css';
+import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
+import App from './App';
+import './index.css';
 
-function App() {
-  return (
-    <div>
-      <Route exact path="/" component={MainPage} />
-      <Route exact path="/search" component={SearchPage} />
-    </div>
-  );
-}
-
-export default App;
+ReactDOM.render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>,
+  document.getElementById('root')
+);
